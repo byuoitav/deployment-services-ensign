335fcd2236a54a313dce360359a4a81b8bb690b4
jakepeery <jake_peery@byu.edu>
2024-06-14 16:20:53 -0600

    fixing deployurl

--- champagne/pi/deploy.go
+++ champagne/pi/deploy.go
@@ -14,7 +14,7 @@ import (
 )
 
 const (
-	DeployURL      = "http://ansible.av.ensign.edu:8080/deploy/"
+	DeployURL      = "http://ansible.avdev.ensign.edu:8080/deploy/"
 	DeploymentFile = "/tmp/deployment.log"
 )
 
