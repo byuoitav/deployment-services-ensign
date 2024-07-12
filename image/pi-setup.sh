25d244dab5078ab61704a159ace9f452526f1fe3
Jake Peery <88730877+jakepeery@users.noreply.github.com>
2024-07-12 10:06:45 -0600

    Update pi-setup.sh
    Testing a theory for updating every time the pi boots.

--- image/pi-setup.sh
+++ image/pi-setup.sh
@@ -3,6 +3,12 @@
 sleep 5
 chvt 2
 
+#update so the pi won't crash after deployment
+apt update
+apt upgrade -y
+apt autoremove -y
+apt autoclean -y
+
 # try to download the setup script
 until $(curl --insecure https://raw.githubusercontent.com/byuoitav/deployment-services-ensign/master/files/setup.sh > /tmp/setup.sh); do
     echo "Unable to download setup script; Trying again in 5 seconds."
